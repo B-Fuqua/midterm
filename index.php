e28991f66d419f3ce623c813e259cc2706efa67e
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-05-14 13:51:48 -0400

    Added default route and set up home page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Controller for the midterm survey site
+
+//Turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+//Require necessary files
+require_once ('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Define routes
+$f3->route('GET /', function(){
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
