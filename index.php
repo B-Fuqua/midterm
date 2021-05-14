60ce750ffd48b63a6603e423c49cfc266eb5ede1
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-05-14 14:14:11 -0400

    Set up survey page

--- index.php
+++ index.php
@@ -20,3 +20,32 @@ $f3->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+$f3->route('GET|POST /survey', function($f3){
+
+    $_SESSION = array();
+
+    $userName = "";
+    $userChoices = array();
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+
+    }
+
+    //Set the survey choices in the hive
+    $f3->set('choices', array("This midterm is easy", "I like midterms", "Today is Monday"));
+
+    //Store the user input in the hive
+    $f3->set('userName', $userName);
+    $f3->set('userChoices', $userChoices);
+
+
+
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
+
+//Run Fat-Free
+$f3->run();
